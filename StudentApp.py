0eba45fbce28092990959577542d5b5e5e6d30c3
Owen Chan <111181579+OwenChan39@users.noreply.github.com>
2023-09-16 21:11:21 +0800

    Update StudentApp.py

--- StudentApp.py
+++ StudentApp.py
@@ -6,5 +6,9 @@ app = Flask(__name__, static_folder='static')
 def home():
     return render_template('index.html')
 
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
