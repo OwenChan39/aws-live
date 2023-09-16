b3e9bd75918568a20ea05234fa6e36e6270f228f
Owen Chan <111181579+OwenChan39@users.noreply.github.com>
2023-09-16 21:02:02 +0800

    Update StudentApp.py

--- StudentApp.py
+++ StudentApp.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 @app.route("/")
-def index():
+def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
