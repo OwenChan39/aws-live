feaed9a462d70d6fe21404a403b3448d32e1c729
Owen Chan <111181579+OwenChan39@users.noreply.github.com>
2023-09-17 13:15:22 +0800

    Update StudentApp.py

--- StudentApp.py
+++ StudentApp.py
@@ -130,81 +130,56 @@ def student_dashboard():
     # If the student is not logged in, redirect to the login page
     return redirect(url_for('login'))
 
-# Function to update the student profile in the database
-def update_student_profile(student_id, cgpa, mobile_number, home_address, personal_email):
-    cursor = db_conn.cursor()
-    update_sql = "UPDATE Student SET CGPA=%s, Mobile_number=%s, Home_Address=%s, Personal_emailAddress=%s WHERE Stud_ID=%s"
-    try:
-        cursor.execute(update_sql, (cgpa, mobile_number, home_address, personal_email, student_id))
-        db_conn.commit()
-        return True  # Profile updated successfully
-    except Exception as e:
-        db_conn.rollback()
-        return str(e)  # Error updating profile
-    finally:
-        cursor.close()
-
 @app.route('/student_profile_edit', methods=['GET', 'POST'])
 def student_profile_edit():
-    if request.method == 'POST':
-        # Check if the form was submitted
-        updated_fields = request.form.getlist('update_fields[]')
+    if 'student_id' in session:
+        student_id = session['student_id']
+        cursor = db_conn.cursor()
+        cursor.execute("SELECT * FROM Student WHERE Stud_ID = %s", (student_id,))
+        student = cursor.fetchone()
+        cursor.close()
 
-        if 'cgpa' in updated_fields:
-            # Update the CGPA field
-            new_cgpa = request.form.get('cgpa')
-            if new_cgpa:
-                if update_student_profile(session['student_id'], new_cgpa, None, None, None) is True:
-                    flash('CGPA updated successfully', 'success')
-                else:
-                    flash('Error updating CGPA', 'error')
+        if student:
+            if request.method == 'POST':
+                updated_fields = request.form.getlist('update_fields[]')
 
-        if 'mobile' in updated_fields:
-            # Update the Mobile Number field
-            new_mobile_number = request.form.get('mobileNumber')
-            if new_mobile_number:
-                if update_student_profile(session['student_id'], None, new_mobile_number, None, None) is True:
-                    flash('Mobile Number updated successfully', 'success')
-                else:
-                    flash('Error updating Mobile Number', 'error')
+                if 'cgpa' in updated_fields:
+                    new_cgpa = request.form.get('cgpa')
+                    if new_cgpa:
+                        if update_student_profile(student_id, new_cgpa, None, None, None) is True:
+                            flash('CGPA updated successfully', 'success')
+                        else:
+                            flash('Error updating CGPA', 'error')
 
-        if 'address' in updated_fields:
-            # Update the Home Address field
-            new_home_address = request.form.get('homeAddress')
-            if new_home_address:
-                if update_student_profile(session['student_id'], None, None, new_home_address, None) is True:
-                    flash('Home Address updated successfully', 'success')
-                else:
-                    flash('Error updating Home Address', 'error')
+                if 'mobile' in updated_fields:
+                    new_mobile_number = request.form.get('mobileNumber')
+                    if new_mobile_number:
+                        if update_student_profile(student_id, None, new_mobile_number, None, None) is True:
+                            flash('Mobile Number updated successfully', 'success')
+                        else:
+                            flash('Error updating Mobile Number', 'error')
 
-        if 'email' in updated_fields:
-            # Update the Personal Email field
-            new_personal_email = request.form.get('personalEmail')
-            if new_personal_email:
-                if update_student_profile(session['student_id'], None, None, None, new_personal_email) is True:
-                    flash('Personal Email updated successfully', 'success')
-                else:
-                    flash('Error updating Personal Email', 'error')
+                if 'address' in updated_fields:
+                    new_home_address = request.form.get('homeAddress')
+                    if new_home_address:
+                        if update_student_profile(student_id, None, None, new_home_address, None) is True:
+                            flash('Home Address updated successfully', 'success')
+                        else:
+                            flash('Error updating Home Address', 'error')
 
-        # After updating the fields, you can redirect the user to the student profile page or any other page.
-        return redirect(url_for('student_dashboard'))
+                if 'email' in updated_fields:
+                    new_personal_email = request.form.get('personalEmail')
+                    if new_personal_email:
+                        if update_student_profile(student_id, None, None, None, new_personal_email) is True:
+                            flash('Personal Email updated successfully', 'success')
+                        else:
+                            flash('Error updating Personal Email', 'error')
 
-    # Render the student profile edit page (GET request)
-    return render_template('try_student_update.html')
+                return redirect(url_for('student_dashboard'))
 
+            return render_template('student_profile_edit.html', student=student)
 
-
-# def update_student_field(student_id, field_name, new_value):
-#     try:
-#         # Update the specified field in the database
-#         cursor = db_conn.cursor()
-#         update_sql = f"UPDATE Student SET {field_name} = %s WHERE Stud_ID = %s"
-#         cursor.execute(update_sql, (new_value, student_id))
-#         db_conn.commit()
-#         cursor.close()
-#         flash(f'Successfully updated {field_name}', 'success')
-#     except Exception as e:
-#         flash(f'Error updating {field_name}: {str(e)}', 'error')
+    return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
