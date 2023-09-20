22f28ce51daa3f791428b5d390071e5d985a6c9a
Owen Chan <111181579+OwenChan39@users.noreply.github.com>
2023-09-20 19:05:28 +0800

    Update StudentApp.py

--- StudentApp.py
+++ StudentApp.py
@@ -361,10 +361,9 @@ def company_profile_edit():
                 else:
                     flash('Error updating company information', 'error')
 
-                return redirect(url_for('company_dashboard'))
+                return render_template('company_info_edit.html', company=company)
 
             return render_template('company_info_edit.html', company=company)
-
     return redirect(url_for('login'))
 
 
